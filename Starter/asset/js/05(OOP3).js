bceb75185fff19f0abce66945068ad429edec892
biruktesfu <it.biruk.tesfaye@gmail.com>
2021-02-19 13:19:21 +0300

    creating an object

--- Starter/asset/js/05(OOP3).js
+++ Starter/asset/js/05(OOP3).js
@@ -1,16 +1,19 @@
-// 1. Create a Person_ES6 Class 
+class Person1 
+{
+constructor (first_name, last_name){
+    this.first_name = first_name
+    this.last_name = last_name
+}
 
 
-
-//2. Create the constructor that initialize firstName and lastName
-
-
-//3. Define greeting() method that displays Full Name
+    greet() {
+        return (`hi ${this.first_name} ${this.last_name}`);
+    }
+}
 
 
 //4. Create  Object using ES6 class by passing firstName,lastName ["Bran Stark"]
-
-
+const Bran = new Person1("Bran", "Stark")
 
 //5. Display the greeting on Bran Object [remove the string when you have the object]
-Obj_C3.innerHTML = "Bran.greeting()";
+Obj_C3.innerHTML = Bran.greet();
--- Starter/asset/js/05(OOP4).js
+++ Starter/asset/js/05(OOP4).js
@@ -4,16 +4,19 @@ function Person(firstName, lastName) {
     this.lastName = lastName;
 }
 
-// 1. Create a getFullName Method on prototype object 
+// 1. Create a getFullName Method on prototype object
+
 // 2. The Function will return firstName lastName
+Person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`
+}
 
-
+Person.prototype.getFullName()
 //create a customer object
 function Customer(firstName, lastName, membership) {
 
     //3. Call the Person Object Constructor using call [Person.call(this,firsName,lastName)]
-
-
+Person.call(this, firstName,lastName)
     this.membership = membership;
 
 }
@@ -21,14 +24,16 @@ function Customer(firstName, lastName, membership) {
 // 4. Set the prototype of Customer to be Person Prototype 
 // Note : Use Object.create
 
+Customer.prototype = Person.prototype
 
 //5. Change the constructor of Customer to be Customer 
 
-
+Customer.prototype.constructor = Customer
 
 // Create Customer Object pass ["Jorah", "Mormont", "PRO"]
 
+const customer = new Customer("Jorah" , "Mormont" , "PRO")
 
 //6. Display The Full Name [remove the string when you have the object]
 
-fun_proto.innerHTML = "custemr_1.getFullName()";
+fun_proto.innerHTML = customer.getFullName();
